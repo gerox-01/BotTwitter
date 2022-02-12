069eb5d2ac79fcfd6e273c4e41471967f6f0da94
David Gerónimo Quiroga Torres <gedaquito@gmail.com>
2022-02-11 19:18:57 -0500

    change key and secrets 👀

--- key.py
+++ key.py
@@ -1,5 +1,5 @@
-consumer_key = 'l9pTxgvghoBkPz1I5sGIWte58'
-consumer_secret = 'hE4OJn8hHlhgoSNCPY0qa2Gj2BtlH2wXlmkdXMnyqROnzR6Pip'
+consumer_key = 'consumer_key'
+consumer_secret = 'consumer_secret'
 
-key = '1492234755412221954-C1Lik0afLrbSoMyR1klNDdqPhdGYlW'
-secret = '56gITWuInLkNF4w0zYr70ElTGpxvp5RRFTz92mA3mmugX'
+key = 'key'
+secret = 'secret'
